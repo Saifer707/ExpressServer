6bb327ec418808abb0b6c75a80b4bed3f4a27150
Saifer707 <pippumellow@hotmail.it>
2017-06-29 16:45:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var sql = require('mysql');
 
 var dbConfig = sql.createConnection({
-	host : '$MYSQL_HOST',
+	host : '172.17.0.3',
 	user : 'developer',
 	password : 'developer',
 	database : 'sampledb'
